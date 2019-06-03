116a0d1acbf7070374c2646413dfcf640479037c
patrickmao93 <patrickmao93@gmail.com>
2019-06-03 20:39:34 +0800

    doing it

---
+++ assets/Script/Block.ts
@@ -0,0 +1,110 @@
+import { BlockTypes } from "./Game";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Block extends cc.Component {
+    @property(cc.SpriteFrame)
+    blockSprite: cc.SpriteFrame = null;
+
+    @property(cc.SpriteFrame)
+    starSprite: cc.SpriteFrame = null;
+
+    @property(cc.SpriteFrame)
+    magnetSprite: cc.SpriteFrame = null;
+
+    public blockType = BlockTypes.Block;
+    public attracted = false;
+
+    private collisionRadius: number;
+    private velocity: cc.Vec2 = cc.v2(0, 0);
+    private game = null;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad() {
+        this.collisionRadius = this.node.width;
+    }
+
+    init(game, type: BlockTypes) {
+        this.game = game;
+
+        switch (type) {
+            case 1:
+                this.node.getComponent(cc.Sprite).spriteFrame = this.blockSprite;
+                this.blockType = BlockTypes.Block;
+                break;
+
+            case 2:
+                this.node.getComponent(cc.Sprite).spriteFrame = this.starSprite;
+                this.blockType = BlockTypes.Star;
+                break;
+
+            case 3:
+                this.node.getComponent(cc.Sprite).spriteFrame = this.magnetSprite;
+                this.blockType = BlockTypes.Magnet;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    start() {}
+
+    update(dt) {
+        if (this.isBlockOutOfBound()) {
+            this.node.destroy();
+        }
+        if (this.attracted) {
+            this.setVelocity(
+                this.game.player.position
+                    .sub(this.node.position)
+                    .normalizeSelf()
+                    .mul(700)
+            );
+        }
+        const dist = this.getPlayerDistance();
+        if (dist < this.collisionRadius) {
+            switch (this.blockType) {
+                case BlockTypes.Block:
+                    console.log("LOST!");
+                    this.game.gameOver();
+                    break;
+
+                case BlockTypes.Star:
+                    this.game.gainScore();
+                    this.node.destroy();
+                    break;
+
+                case BlockTypes.Magnet:
+                    this.node.dispatchEvent(new cc.Event.EventCustom("magnet", true));
+                    this.node.destroy();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        const newX = this.node.position.x + this.velocity.x * dt;
+        const newY = this.node.position.y + this.velocity.y * dt;
+        this.node.setPosition(cc.v2(newX, newY));
+    }
+
+    setVelocity = (v: cc.Vec2): void => {
+        this.velocity = v;
+    };
+
+    isBlockOutOfBound(): boolean {
+        return (
+            Math.abs(this.node.position.x) > this.node.parent.width / 2 + this.node.width / 2 ||
+            this.node.position.y < -this.node.parent.height / 2 - this.node.height / 2
+        );
+    }
+
+    getPlayerDistance() {
+        const playerPos = this.game.player.position;
+        const dist = this.node.position.sub(playerPos).mag();
+        return dist;
+    }
+}
---
+++ assets/Script/Game.ts
@@ -0,0 +1,157 @@
+const { ccclass, property } = cc._decorator;
+
+export enum BlockTypes {
+    Block = 1,
+    Star = 2,
+    Magnet = 3
+}
+
+@ccclass
+export default class Game extends cc.Component {
+    @property(cc.Node)
+    player: cc.Node = null;
+
+    @property(cc.Prefab)
+    BlockPrefab: cc.Prefab = null;
+
+    @property(cc.Label)
+    scoreLabel: cc.Label = null;
+
+    @property
+    moveSpeedMultiplyer: number = 1.2;
+
+    @property
+    blockSpeedMultiplyer: number = 10;
+
+    @property
+    maxBlockSpeed: number = 200;
+
+    @property
+    minBlockSpeed: number = 140;
+
+    @property
+    magnetChance: number = 0.01;
+
+    @property
+    starChance: number = 0.1;
+
+    private time: number;
+    private blockPool = new cc.NodePool();
+    private score = 0;
+    private blocks = [];
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad() {
+        this.registerTouchMove();
+        this.time = 1;
+        this.score = 0;
+
+        for (let i = 0; i < 100; i++) {
+            let block = cc.instantiate(this.BlockPrefab);
+            this.blockPool.put(block);
+        }
+
+        this.node.on("magnet", e => {
+            e.stopPropagation();
+            const children = this.node.children;
+            console.log("children", children);
+            children.forEach(child => {
+                const component = child.getComponent("Block");
+                if (component && component.blockType === BlockTypes.Star) {
+                    component.attracted = true;
+                }
+            });
+        });
+    }
+
+    start() {}
+
+    update(dt) {
+        this.time += dt;
+
+        const roll = Math.random();
+        const chance = 0.001 * this.time ** 1.2;
+        if (roll < chance) {
+            console.log(chance);
+            this.spawnBlock(this.getBlockSpawnPosition(), BlockTypes.Block);
+
+            const magnetRoll = Math.random();
+            const starRoll = Math.random();
+
+            if (starRoll < this.starChance) {
+                this.spawnBlock(this.getBlockSpawnPosition(), BlockTypes.Star);
+            }
+
+            if (magnetRoll < this.magnetChance) {
+                this.spawnBlock(this.getBlockSpawnPosition(), BlockTypes.Magnet);
+            }
+        }
+        this.handlePlayerOutOfbound();
+    }
+
+    handlePlayerOutOfbound() {
+        const playerX = this.player.position.x;
+        const playerY = this.player.position.y;
+        const boundries = { x: this.node.width / 2, y: this.node.height / 2 };
+        if (playerX < -boundries.x) {
+            this.player.setPosition(-boundries.x, playerY);
+        }
+        if (playerX > boundries.x) {
+            this.player.setPosition(boundries.x, playerY);
+        }
+        if (playerY < -boundries.y) {
+            this.player.setPosition(playerX, -boundries.y);
+        }
+        if (playerY > boundries.y) {
+            this.player.setPosition(playerX, boundries.y);
+        }
+    }
+
+    registerTouchMove = () => {
+        this.node.on("touchmove", e => {
+            let delta = e.touch.getDelta();
+            const playerX = this.player.position.x;
+            const playerY = this.player.position.y;
+
+            const newX = playerX + delta.x * this.moveSpeedMultiplyer;
+            const newY = playerY + delta.y * this.moveSpeedMultiplyer;
+            this.player.setPosition(newX, newY);
+        });
+    };
+
+    spawnBlock(pos: cc.Vec2, type: BlockTypes) {
+        let newBlock;
+        if (this.blockPool.size() > 0) {
+            newBlock = this.blockPool.get(this);
+        } else {
+            newBlock = cc.instantiate(this.BlockPrefab);
+        }
+        newBlock.getComponent("Block").init(this, type);
+        const velocity = this.getBlockVelocity();
+        newBlock.getComponent("Block").setVelocity(velocity);
+        this.node.addChild(newBlock);
+        newBlock.setPosition(pos);
+    }
+
+    getBlockVelocity(): cc.Vec2 {
+        return cc.v2(
+            (Math.random() - 0.5) * this.minBlockSpeed,
+            -this.minBlockSpeed + (Math.random() - 1) * (this.maxBlockSpeed - this.minBlockSpeed)
+        );
+    }
+
+    getBlockSpawnPosition(): cc.Vec2 {
+        return cc.v2((Math.random() - 0.5) * this.node.width, this.node.height / 2 + 30);
+    }
+
+    gameOver() {
+        cc.director.pause();
+        this.node.off("touchmove");
+    }
+
+    gainScore() {
+        this.score += 1;
+        this.scoreLabel.string = "Score: " + this.score;
+    }
+}
